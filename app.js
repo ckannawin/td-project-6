ccf9e9bbbc6d60c2804296f7397733a05bdcba6f
ckannawin <64106695+ckannawin@users.noreply.github.com>
2020-11-08 13:35:39 -0600

    Append Questions and Answers to DOM

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const qwerty = document.querySelector ('#qwerty');
 const phrase = document.querySelector ('#phrase');
 const questionArray = ['When does a Skeleton Laugh?', 'What do skeletons hate the most about wind?', 'Why are graveyards so noisy?','Why was the skeleton so upset when the dog took his bone?','Why are skeletons so sad?'];
 const phrasesArray = ['when someone tickles his funny bone', 'Nothing it goes right through them', 'Because of all the coffin','he did not have a leg to stand on', 'they have no body'];
-
+var randomIndex = Math.floor(Math.random() * questionArray.length);
 
 var missed = 0
 
@@ -15,13 +15,54 @@ startButton.addEventListener('click', () => {
 
 function getRandomPhrasesArray (arrP) {
 
-var randomIndex = Math.floor(Math.random() * arrP.length);
-var randomPhrase = arrP[randomIndex];
-console.log(randomPhrase);
-return;
+    var randomPhrase = arrP[randomIndex];
+    //console.log(randomPhrase);
 
+    var phraseParse = Object.values(randomPhrase)
+    //console.log(phraseParse);
+    return phraseParse;
 }
 
-getRandomPhrasesArray(phrasesArray, questionArray);
+function getRandomQuestionsArray (arrQ) {
+    var randomQuestion = arrQ[randomIndex];
+    //console.log(randomQuestion);
+
+    return randomQuestion;
+    }
+function addQuestionToDisplay(arr) {
+    
+        let appendQuestion = document.createElement('h3');
+        appendQuestion.textContent = arr;
+        document.querySelector('#banner').appendChild(appendQuestion);
+        appendQuestion.className = "questions";
+    
+}
 
 
+
+//getRandomPhrasesArray(phrasesArray);
+//getRandomQuestionsArray(questionArray);
+
+function addPhraseToDisplay(arr) {
+    for (i = 0; i <= arr.length; i++) {
+        let letterItem = document.createElement('li');
+        letterItem.textContent = arr[i];
+        if (letterItem.textContent === " ") {
+            letterItem.className = "space";
+        } else if (letterItem.textContent === ""){
+            letterItem.className = "space";
+        } else {
+            letterItem.className = "letter";
+        }
+        //console.log(letterItem);
+        phrase.appendChild(letterItem);
+    }
+   
+}
+
+const question = getRandomQuestionsArray (questionArray);
+const phrasesParse = getRandomPhrasesArray(phrasesArray);
+console.log(phrasesParse);
+console.log(question);
+addPhraseToDisplay(phrasesParse);
+addQuestionToDisplay(question);
