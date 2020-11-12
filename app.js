e68da64a50af085d3a68f66c1596e4ea6f113a95
ckannawin <64106695+ckannawin@users.noreply.github.com>
2020-11-11 22:56:51 -0600

    Life Count and win/loss Function

--- app.js
+++ app.js
@@ -3,8 +3,9 @@ const startButton = document.querySelector('.btn__reset');
 const overlay = document.querySelector('#overlay');
 const qwerty = document.querySelector ('#qwerty');
 const phrase = document.querySelector ('#phrase');
-const questionArray = ['When does a Skeleton Laugh?', 'What do skeletons hate the most about wind?', 'Why are graveyards so noisy?','Why was the skeleton so upset when the dog took his bone?','Why are skeletons so sad?'];
-const phrasesArray = ['when someone tickles his funny bone', 'Nothing it goes right through them', 'Because of all the coffin','he did not have a leg to stand on', 'they have no body'];
+const tries = document.querySelectorAll('.tries');
+const questionArray = ['How do you make a Skeleton Laugh?', 'What do skeletons hate the most about wind?', 'Why are graveyards so noisy?','Why was the skeleton so upset when the dog took his bone?','Why are skeletons so sad?'];
+const phrasesArray = ['tickle their funny bone', 'it goes right through them', 'all the coffin','took his leg to stand on', 'they have no body'];
 var randomIndex = Math.floor(Math.random() * questionArray.length);
 
 var missed = 0
@@ -55,8 +56,6 @@ function addPhraseToDisplay(arr) {
 
 const question = getRandomQuestionsArray (questionArray);
 const phrasesParse = getRandomPhrasesArray(phrasesArray);
-// console.log(phrasesParse);
-// console.log(question);
 addPhraseToDisplay(phrasesParse);
 addQuestionToDisplay(question);
 
@@ -92,8 +91,34 @@ qwerty.addEventListener ('click', (e) => {
 
         checkLetter(e.target);
         let letterFound = checkLetter(e.target);
+        
+        if (letterFound === null) {
+            missed += 1;
+            const ol = document.querySelector('#scoreboard-list');
+            ol.removeChild(tries[missed - 1]);
+        } 
     }
-    
+    checkWin();
 });
+
+function checkWin() {
+
+    let letter = document.querySelectorAll('.letter');
+    let show = document.querySelectorAll('.show');
+
+    if ( letter.length === show.length ) {
+        
+        overlay.className = 'win';
+        overlay.style.display = 'flex';
+
+        document.querySelector('h2').textContent = 'You are a Bone-afide winner!';
+    } else if (missed >= 5) { 
+
+        overlay.className = 'lose';
+        overlay.style.display = 'flex';
+
+        document.querySelector('h2').textContent = 'Try again, its a skele-ton of fun!';
+    }
+};
 
 
