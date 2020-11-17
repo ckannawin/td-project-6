e58ddcd52e94ac3a781df7c87e2a4d7e58c5c7c8
ckannawin <64106695+ckannawin@users.noreply.github.com>
2020-11-16 20:45:44 -0600

    Ready for Review

--- app.js
+++ app.js
@@ -8,26 +8,27 @@ const questionArray = ['How do you make a Skeleton Laugh?', 'What do skeletons h
 const phrasesArray = ['tickle funny bone', 'it goes through them', 'all the coffin','no leg to stand on', 'they have no body'];
 
 var randomIndex = Math.floor(Math.random() * questionArray.length);
- 
-
 var missed = 0;
 
+//Attach a event listener to the “Start Game” button to hide the start screen overlay.
 startButton.addEventListener('click', () => {
     overlay.style.display = 'none';
 });
 
+//Create a getRandomPhraseAsArray function.
 function getRandomPhrasesArray (arrP) {
 
     var randomPhrase = arrP[randomIndex];
     var phraseParse = Object.values(randomPhrase);
     return phraseParse;
 }
-
+//Function to retrieve corresponding question
 function getRandomQuestionsArray (arrQ) {
     var randomQuestion = arrQ[randomIndex];
     return randomQuestion;
 }
 
+//Add question as h3 element
 function addQuestionToDisplay(arr) {
     let appendQuestion = document.createElement('h3');
     appendQuestion.textContent = arr;
@@ -35,6 +36,7 @@ function addQuestionToDisplay(arr) {
     appendQuestion.className = "questions";
 }
 
+//Create an addPhraseToDisplay function that loops through an array of characters.
 function addPhraseToDisplay(arr) {
     for (i = 0; i <= arr.length; i++) {
         let letterItem = document.createElement('li');
@@ -52,12 +54,14 @@ function addPhraseToDisplay(arr) {
    
 }
 
-const question = getRandomQuestionsArray (questionArray);
+//Save array values as variables and pass to functions to add to display
+const question = getRandomQuestionsArray(questionArray);
 const phrasesParse = getRandomPhrasesArray(phrasesArray);
 addPhraseToDisplay(phrasesParse);
 addQuestionToDisplay(question);
 
-
+//Create Check letter function Elements with a class of “letter”
+//The function should loop over the letters and check if they match the letter in the button the player has chosen.
 function checkLetter (button) {
 
     let letterSelector =  document.querySelectorAll('.letter');
@@ -78,6 +82,8 @@ function checkLetter (button) {
     return match;
 };
 
+//Add event Listener to listen to buttons in keyboards. Pass class "Chosen"
+//Upon missed click, increment missed counter and remove heart
 qwerty.addEventListener ('click', (e) => {
 
     if (e.target.tagName === 'BUTTON') {
@@ -96,6 +102,8 @@ qwerty.addEventListener ('click', (e) => {
     checkWin();
 });
 
+//Each time the player guesses a letter, this function will check whether the game has been won or lost
+//If # of misses reaches >5 or phrase is guessed in full, display win/loss screen and run reset function
 function checkWin() {
 
     let letter = document.querySelectorAll('.letter');
@@ -108,7 +116,7 @@ function checkWin() {
 
         document.querySelector('h2').textContent = 'You are a Bone-afide winner!';
 
-        randomIndex += 1;
+        randomIndex = Math.floor(Math.random() * questionArray.length);
 
         reset();
     } else if (missed >= 5) { 
@@ -118,15 +126,19 @@ function checkWin() {
 
         document.querySelector('h2').textContent = 'Try again, its a skele-ton of fun!';
 
+        randomIndex = Math.floor(Math.random() * questionArray.length);
+
         reset();
     }
 
     
 };
 
+//reset function upon start button click. Resets the missed count, removes current question h3 and empties phrase content
+//refills heart containers and deselects .chosen classes, adds new question and phrase
 function reset() {
     startButton.addEventListener('click', () => {
-        overlay.className = 'start';
+        
         missed = 0;
 
         const banner = document.querySelector('#banner');
@@ -148,7 +160,6 @@ function reset() {
             chosenDeselect[i].classList.remove('chosen');
             chosenDeselect[i].disabled = false;
         };
-        var randomIndex = Math.floor(Math.random() * questionArray.length);
         
         const question = getRandomQuestionsArray (questionArray);
         const phrasesParse = getRandomPhrasesArray(phrasesArray);
